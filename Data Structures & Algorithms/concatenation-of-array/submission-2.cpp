993c692387a1da6a441f1e7ccae46efe03652ff7
U1m2 <92878087+U1m2@users.noreply.github.com>
2026-05-14 12:55:25 +0530

    Add: concatenation-of-array - submission-2

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-2.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        /*
+        //random memory increase
+        int actualsize = nums.size();
+
+        for(int i=0;i<actualsize;i++)
+        {
+            nums.push_back(nums[i]);
+        }
+        return nums;
+        */
+        /* Reserving causes increase in runtime
+        int n = nums.size();
+
+        nums.reserve(2*n);
+        for(int i=0;i<n;i++)
+        {
+            nums.push_back(nums[i]);
+        }
+        return nums;
+        */
+        int n =nums.size();
+        vector<int> newnums(2*n);
+        for(int i=0;i<n;i++)
+        { 
+            newnums[i] = nums[i];
+            newnums[i+n] = nums[i];
+        }
+        return newnums;
+    }
+};
