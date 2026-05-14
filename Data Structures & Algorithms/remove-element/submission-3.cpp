d24edc4b63bb4a4090115258f2fec6ccad80e42d
U1m2 <92878087+U1m2@users.noreply.github.com>
2026-05-14 11:02:09 +0530

    Add: remove-element - submission-3

---
+++ Data Structures & Algorithms/remove-element/submission-3.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        
+        int k=0;
+        for(auto &i: nums)
+        {
+            if(i != val)
+            {
+                nums[k++] =i;
+            }
+        }
+        return k;
+        
+        /*
+        int n = nums.size();
+        int i=0; // starting index of the array
+        while(i< n)
+        {
+            if(nums[i]== val) 
+            {
+                nums[i] = nums[n-1];
+                n= n-1; //Replacing the last element of an array to the current.
+                // Not incrementing the current index because this needs to be 
+                // re-evaluated.
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return n ; // this is the count of valid element
+        */
+    }
+};
