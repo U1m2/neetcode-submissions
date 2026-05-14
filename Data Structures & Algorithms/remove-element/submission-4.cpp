628a26ba54ee0a77f78505361a145a5fb51801c8
U1m2 <92878087+U1m2@users.noreply.github.com>
2026-05-14 18:15:39 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/remove-element/submission-4.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        /*
+        //runtime  2ms with O(n) Time complexity to visit all the elements of an
+        //array
+        int k=0;
+        for(auto &i: nums)
+        {
+            if(i != val)
+            {
+                nums[k++] =i;
+            }
+        }
+        return k;
+        */
+        
+        /*  
+        //runtime is 3ms , O(N) complexity but worstcase of all elements same as val
+        // multiple overwrite happens.
+        int n = nums.size();
+        int i=0; // starting index of the array
+        while(i< n)
+        {
+            if(nums[i]== val) 
+            {
+                nums[i] = nums[n-1];
+                n= n-1; //Replacing the last element of an array to the current.
+                // Not incrementing the current index because this needs to be 
+                // re-evaluated.
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return n ; // this is the count of valid element
+        */
+    }
+};
